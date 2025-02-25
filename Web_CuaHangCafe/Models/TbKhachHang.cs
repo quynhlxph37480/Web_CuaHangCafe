7d51884483db7f24749b8d314205b25d5f26e477
MSI\MSII <quynhlxph37480@fpt.edu.vn>
2025-02-25 23:38:43 +0700

    Add project files.

---
+++ Web_CuaHangCafe/Models/TbKhachHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_CuaHangCafe.Models;
+
+public partial class TbKhachHang
+{
+    public Guid Id { get; set; }
+
+    public string TenKhachHang { get; set; } = null!;
+
+    public string SdtkhachHang { get; set; } = null!;
+
+    public string DiaChi { get; set; } = null!;
+
+    public virtual ICollection<TbHoaDonBan> TbHoaDonBans { get; set; } = new List<TbHoaDonBan>();
+}
